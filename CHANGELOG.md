a5a51e1766db78afd3216b6f562bbe48f8c94d23
Grégory NEUT <gregory.neut@gmail.com>
2022-04-22 16:58:13 +0200

    add set_governance_delegate instruction

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # CHANGELOG
 
+## v0.2.0
+
+1. Add SplGovernanceV2 support with :
+   - DepositGoverningTokens
+   - WithdrawGoverningTokens
+   - CastVote
+   - RelinquishVote
+
 ## v0.1.0
 
 1. Add MangoMarketsV3 support with :
--- programs/anchor-comp/src/spl_governance_v2.rs
+++ programs/anchor-comp/src/spl_governance_v2.rs
@@ -8,12 +8,47 @@ mod spl_governance_program_id {
     solana_program::declare_id!("GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw");
 }
 
+#[derive(Clone)]
+pub struct SplGovernanceV2;
+
+impl anchor_lang::Id for SplGovernanceV2 {
+    fn id() -> Pubkey {
+        spl_governance_program_id::ID
+    }
+}
+
 // Utility function
 fn get_pubkey_from_opt_account_info(opt_account_info: Option<&AccountInfo>) -> Option<Pubkey> {
     return match opt_account_info {
         Some(account_info) => Some(*account_info.key),
         None => None,
     };
+}
+
+pub fn set_governance_delegate<'a, 'b, 'c, 'info>(
+    ctx: CpiContext<'a, 'b, 'c, 'info, SetGovernanceDelegate<'info>>,
+) -> Result<()> {
+    check_program_account(ctx.program.key)?;
+
+    let mut remaining_accounts_iter = ctx.remaining_accounts.iter();
+
+    let new_governance_delegate = get_pubkey_from_opt_account_info(remaining_accounts_iter.next());
+
+    let ix = spl_governance::instruction::set_governance_delegate(
+        &spl_governance_program_id::ID,
+        ctx.accounts.governance_authority.key,
+        ctx.accounts.governance_realm_account.key,
+        ctx.accounts.governing_token_mint.key,
+        ctx.accounts.governing_token_owner.key,
+        &new_governance_delegate,
+    );
+
+    solana_program::program::invoke_signed(
+        &ix,
+        &ToAccountInfos::to_account_infos(&ctx),
+        ctx.signer_seeds,
+    )
+    .map_err(Into::into)
 }
 
 pub fn deposit_governing_tokens<'a, 'b, 'c, 'info>(
@@ -122,6 +157,18 @@ pub fn relinquish_vote<'a, 'b, 'c, 'info>(
         ctx.signer_seeds,
     )
     .map_err(Into::into)
+}
+
+#[derive(Accounts)]
+pub struct SetGovernanceDelegate<'info> {
+    /// CHECK: Spl Governance CPI
+    pub governance_authority: AccountInfo<'info>,
+    /// CHECK: Spl Governance CPI
+    pub governance_realm_account: AccountInfo<'info>,
+    /// CHECK: Spl Governance CPI
+    pub governing_token_mint: AccountInfo<'info>,
+    /// CHECK: Spl Governance CPI
+    pub governing_token_owner: AccountInfo<'info>,
 }
 
 #[derive(Accounts)]
